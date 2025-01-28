0558eac5bb069df471e4cdb533f1d8ae154f9842
sergey.shilov <serghspm@inbox.ru>
2025-01-28 19:48:32 +0300

    Build fix

--- src/components/Forms/form_elements/DatePicker.tsx
+++ src/components/Forms/form_elements/DatePicker.tsx
@@ -1,31 +1,43 @@
-'use client'
-// import { cn } from "@/lib/utils"
-import { format } from "date-fns"
-import { Calendar } from "@/components/ui/calendar"
+'use client';
+
+import { format } from 'date-fns';
+import { Calendar } from '@/components/ui/calendar';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { useState } from "react"
-import { Input } from "../../ui/input"
-import { cn } from "@/lib/utils"
+} from '@/components/ui/popover';
+import { useState } from 'react';
+import { Input } from '../../ui/input';
+import { cn } from '@/lib/utils';
 
 interface IDatePickerProps {
-  nameAttr: string,
-  defaultValue?: string | number | Date,
-  isError?: boolean
+  nameAttr: string;
+  defaultValue?: string | number | Date;
+  isError?: boolean;
 }
 
-const DatePicker = ({ nameAttr, defaultValue = '', isError = false }: IDatePickerProps) => {
-  const [date, setDate] = useState<Date>()
+const DatePicker = ({
+  nameAttr,
+  defaultValue = '',
+  isError = false,
+}: IDatePickerProps) => {
+  const [date, setDate] = useState<Date>();
 
-  const dateValue = date ? format(date, 'dd.MM.yyyy') : defaultValue ? format(defaultValue, 'dd.MM.yyyy') : ''
+  const dateValue =
+    date || defaultValue ? format(date || defaultValue, 'dd.MM.yyyy') : '';
 
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Input type="text" value={dateValue} placeholder='ДД.ММ.ГГГГ' name={nameAttr} readOnly className={cn(isError && 'border-destructive')} />
+        <Input
+          type="text"
+          value={dateValue}
+          placeholder="ДД.ММ.ГГГГ"
+          name={nameAttr}
+          readOnly
+          className={cn(isError && 'border-destructive')}
+        />
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
         <Calendar
@@ -37,6 +49,6 @@ const DatePicker = ({ nameAttr, defaultValue = '', isError = false }: IDatePicke
       </PopoverContent>
     </Popover>
   );
-}
+};
 
 export default DatePicker;
