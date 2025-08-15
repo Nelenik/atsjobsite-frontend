5e6b3efae508fb4bacc8fb9b49c000dac056ab78
NeLeNik <lnek10.1990@gmail.com>
2025-08-15 17:47:15 +0300

    feat: add google analytics

--- src/widgets/analytics/ui/Analytics.tsx
+++ src/widgets/analytics/ui/Analytics.tsx
@@ -1,5 +1,5 @@
 import { YANDEX_ID, YandexMetrikaInit } from "@/features/metrika-yandex";
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const Analytics = () => {
   if (process.env.NODE_ENV !== 'production') return null
@@ -9,6 +9,7 @@ export const Analytics = () => {
         id={YANDEX_ID}
         initParameters={{ ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", accurateTrackBounce: true, trackLinks: true }}
       />
+      <GoogleAnalytics gaId="G-P6TLBK16LT" />
     </>
   );
 }
