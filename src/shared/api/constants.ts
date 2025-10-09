5f741d9b395177cb4a6c0be2fec1df4f3a1bbb56
NeLeNik <lnek10.1990@gmail.com>
2025-10-09 03:21:47 +0300

    refactor: changed api url

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -7,7 +7,8 @@ export const mutationInitialState: TMutationState = {
 
 export const API_URL = process.env.API_URL;
 
-export const FILE_STORE_BASE_URL = "https://admin.rekrutai.ru";
+// export const FILE_STORE_BASE_URL = "https://admin.rekrutai.ru";
+export const FILE_STORE_BASE_URL = "https://rekrut.ing";
 
 export const AUTH_COOKIE_NAME = "access_token";
 
