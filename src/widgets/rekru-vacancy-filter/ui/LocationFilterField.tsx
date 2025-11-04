6277311532a3c5eee26bbe84052ba50f446f695a
NeLeNik <lnek10.1990@gmail.com>
2025-11-04 12:38:59 +0200

    refactor: added debounce to the location field, style improvements

--- src/widgets/rekru-vacancy-filter/ui/LocationFilterField.tsx
+++ src/widgets/rekru-vacancy-filter/ui/LocationFilterField.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query"
 import { searchAreasByName } from "@/features/add-hh-matches/api/areasActions"
 import { useState } from "react"
 import { AutocompleteControlledField } from "@/shared/ui/form-elements/autocomplete-field.tsx"
+import { useDebounce } from "@/shared/model/hooks/useDebounce"
+import { Search } from "lucide-react"
 
 type TProps = {
   defaultValue?: string
@@ -16,34 +18,38 @@ export const LocationFilterField = ({
 }: TProps) => {
   const [searchText, setSearchText] = useState(defaultValue)
 
-  // useEffect(() => {
-  //   setSearchText(defaultValue)
-  // }, [defaultValue])
+  const debouncedSearchText = useDebounce(searchText, 300)
 
   // Get areas based on search text
   const { data: searchLocations = [], isFetching } = useQuery({
-    queryKey: ['search-locations', searchText],
-    queryFn: () => searchAreasByName(searchText),
-    enabled: searchText.length >= 3
+    queryKey: ['search-locations', debouncedSearchText],
+    queryFn: () => searchAreasByName(debouncedSearchText),
+    enabled: debouncedSearchText.length >= 2
   })
 
-  console.log(searchLocations)
-  console.log('isFetching', isFetching)
   return (
     <FilterBase
       triggerText="Город"
       onSave={() => updateCb({ location: searchText })}
       onCancel={() => updateCb({ location: '' })}
+      disableSave={searchText.length < 2}
     >
 
-      <AutocompleteControlledField
-        value={searchText}
-        suggestionsList={searchLocations.map(item => item.name)}
-        isFetching={isFetching}
-        onChange={(e) => setSearchText(e.target.value)}
-        onItemSelect={(value) => setSearchText(value)}
-        popoverStyles="p-4"
-      />
+      <div className="relative">
+        <Search
+          className="absolute top-1/2 left-2 -translate-y-1/2"
+        />
+        <AutocompleteControlledField
+          value={searchText}
+          suggestionsList={searchLocations.map(item => item.name)}
+          isFetching={isFetching}
+          onChange={(e) => setSearchText(e.target.value)}
+          onItemSelect={(value) => setSearchText(value)}
+          popoverStyles="p-4"
+          placeholder="Введите город, область или страну"
+          className='pl-10'
+        />
+      </div>
     </FilterBase>
   );
 }
